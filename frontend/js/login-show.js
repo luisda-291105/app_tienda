93a773c27f5842dfd6be90c5fcb7d0c6361f9df1
luisDaniel <estudiosluisperez@gmail.com>
2026-03-02 17:25:54 -0500

    style se le agrego comentarios que dicen lo que hace la funcion

--- frontend/js/login-show.js
+++ frontend/js/login-show.js
@@ -1,3 +1,4 @@
+// variable nombre de usuario en la barra de navegacion ,  es el perfil
 const nameUser = document.querySelector("#userDropdown span")
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -7,11 +8,13 @@ document.addEventListener("DOMContentLoaded", () => {
     console.table(user);
 });
 
+// funcion que trae y guarda en una variable el usuario que inicio
 function collactUsersDataLogin() {
     let userFound = JSON.parse(localStorage.getItem("userFound"));
     return userFound;
 }
 
+// funcion que muestra el usuario que inicio
 function showUser(user) {
     nameUser.textContent = user.firsName;
 }
